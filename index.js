d4075044fc27ce7ad809f318bbad527fb84205bf
Anthony Rodriguez <rdz.tony1@gmail.com>
2022-09-04 00:41:18 -0500

    commit tree 525db0885bc7bef9e28e70ae991e34d3bea5fda1

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Add your code here
+
+/*const addDogs={
+    dogName: 'Byron',
+    dogBreed: 'Poodle',
+};
+
+const configurationObject = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    },
+    body: JSON.stringify(addDogs),
+
+};
+
+
+
+fetch('http://localhost:3000/dogs', configurationObject)
+.then(resp=>resp.json())
+.then(data=>console.log(data))
+.catch(error=>{
+    alert('Bad things! Ragnarok!');
+    console.log(error.message);
+});
+*/
+
+
+function submitData(userName, UserEmail) {
+
+
+const addUsers = {
+    name: userName,
+    email: UserEmail
+};
+//console.log(JSON.stringify(addUsers))
+
+const configObjectUsers = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify(addUsers)
+};
+
+
+return fetch('http://localhost:3000/users', configObjectUsers)
+.then(resp=>resp.json())
+.then(data=>console.log(data))
+.catch(error=>{
+    alert('Ooops, something went wrong')
+    console.log(error.message)})
+
+
+};
+
+//submitData('Tony', '123tony@domain.com')
+submitData('Tony', 'heytony@gmail')
+
+
